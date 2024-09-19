e5e55a7ed2b1938000be84c119ef0f06b40a84ea
SuperMeatBoyCoder <79085115+SuperMeatBoyCoder@users.noreply.github.com>
2024-09-19 20:02:38 +0300

    Create z_func, p_func.cpp

---
+++ z_func, p_func.cpp
@@ -0,0 +1,16 @@
+vector<int> z_func(string s) {
+    int n = s.size();
+    vector<int> z(n);
+    int l = 0, r = 0;
+    for (int i = 1; i < n; i++) {
+        if (i < r)
+            z[i] = min(r - i, z[i - l]);
+        while (i + z[i] < n && s[i + z[i]] == s[z[i]]) {
+            z[i]++;
+        }
+        if (i + z[i] > r) {
+            l = i, r = z[i] + i;
+        }
+    }
+    return z;
+}
