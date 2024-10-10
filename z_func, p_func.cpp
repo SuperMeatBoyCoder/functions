55ee03bac20c5125db312a3d308230ea7819df3a
SuperMeatBoyCoder <79085115+SuperMeatBoyCoder@users.noreply.github.com>
2024-10-10 18:37:36 +0300

    Update z_func, p_func.cpp

--- z_func, p_func.cpp
+++ z_func, p_func.cpp
@@ -14,3 +14,17 @@ vector<int> z_func(string s) {
     }
     return z;
 }
+
+vector<int> p_func(string s) {
+    int n = s.size();
+    vector<int> p(n + 1);
+    p[0] = -1;
+    for (int i = 1; i <= n; i++) {
+        int k = p[i - 1];
+        while (k != -1 && s[k] != s[i - 1]) {
+            k = p[k];
+        }
+        p[i] = ++k;
+    }
+    return p;
+}
